fb21180723033f7c95365be934900811dd49ffc9
Andrew Gasser <agasser01@hotmail.com>
2023-12-08 14:49:51 -0600

    Formatting and standards

--- src/main/java/frc/robot/Telemetry.java
+++ src/main/java/frc/robot/Telemetry.java
@@ -19,113 +19,118 @@ import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 
 public class Telemetry {
-    private final double MaxSpeed;
-    private int logEntry;
-    private int odomEntry;
+  private final double maxSpeed;
+  private final int logEntry;
+  private final int odomEntry;
 
-    /**
-     * Construct a telemetry object, with the specified max speed of the robot
-     * 
-     * @param maxSpeed Maximum speed in meters per second
-     */
-    public Telemetry(double maxSpeed) {
-        MaxSpeed = maxSpeed;
-        logEntry = DataLogManager.getLog().start("odometry", "double[]");
-        odomEntry = DataLogManager.getLog().start("odom period", "double");
+  /* What to publish over networktables for telemetry */
+  private final NetworkTableInstance inst = NetworkTableInstance.getDefault();
+
+  /* Robot pose for field positioning */
+  private final NetworkTable table = inst.getTable("Pose");
+  private final DoubleArrayPublisher fieldPub = table.getDoubleArrayTopic("robotPose").publish();
+  private final StringPublisher fieldTypePub = table.getStringTopic(".type").publish();
+
+  /* Robot speeds for general checking */
+  private final NetworkTable driveStats = inst.getTable("Drive");
+  private final DoublePublisher velocityX = driveStats.getDoubleTopic("Velocity X").publish();
+  private final DoublePublisher velocityY = driveStats.getDoubleTopic("Velocity Y").publish();
+  private final DoublePublisher speed = driveStats.getDoubleTopic("Speed").publish();
+  private final DoublePublisher odomPeriod = driveStats.getDoubleTopic("Odometry Period").publish();
+
+  /* Mechanisms to represent the swerve module states */
+  private final Mechanism2d[] moduleMechanisms = new Mechanism2d[] {
+      new Mechanism2d(1, 1),
+      new Mechanism2d(1, 1),
+      new Mechanism2d(1, 1),
+      new Mechanism2d(1, 1),
+  };
+
+  /* A direction and length changing ligament for speed representation */
+  private final MechanismLigament2d[] moduleSpeeds = new MechanismLigament2d[] {
+      moduleMechanisms[0].getRoot("RootSpeed", 0.5, 0.5).append(new MechanismLigament2d("Speed", 0.5, 0)),
+      moduleMechanisms[1].getRoot("RootSpeed", 0.5, 0.5).append(new MechanismLigament2d("Speed", 0.5, 0)),
+      moduleMechanisms[2].getRoot("RootSpeed", 0.5, 0.5).append(new MechanismLigament2d("Speed", 0.5, 0)),
+      moduleMechanisms[3].getRoot("RootSpeed", 0.5, 0.5).append(new MechanismLigament2d("Speed", 0.5, 0)),
+  };
+
+  /* A direction changing and length constant ligament for module direction */
+  private final MechanismLigament2d[] moduleDirections = new MechanismLigament2d[] {
+      moduleMechanisms[0].getRoot("RootDirection", 0.5, 0.5)
+          .append(new MechanismLigament2d("Direction", 0.1, 0, 0, new Color8Bit(Color.kWhite))),
+      moduleMechanisms[1].getRoot("RootDirection", 0.5, 0.5)
+          .append(new MechanismLigament2d("Direction", 0.1, 0, 0, new Color8Bit(Color.kWhite))),
+      moduleMechanisms[2].getRoot("RootDirection", 0.5, 0.5)
+          .append(new MechanismLigament2d("Direction", 0.1, 0, 0, new Color8Bit(Color.kWhite))),
+      moduleMechanisms[3].getRoot("RootDirection", 0.5, 0.5)
+          .append(new MechanismLigament2d("Direction", 0.1, 0, 0, new Color8Bit(Color.kWhite))),
+  };
+
+  private final DoubleArrayPublisher moduleStatePublisher = driveStats.getDoubleArrayTopic("Module States").publish();
+
+  /* Keep a reference of the last pose to calculate the speeds */
+  private Pose2d lastPose = new Pose2d();
+  private double lastTime = Utils.getCurrentTimeSeconds();
+
+  /**
+   * Construct a telemetry object, with the specified max speed of the robot
+   * 
+   * @param maxSpeed
+   *          Maximum speed in meters per second
+   */
+  public Telemetry(double maxSpeed) {
+    this.maxSpeed = maxSpeed;
+    logEntry = DataLogManager.getLog().start("odometry", "double[]");
+    odomEntry = DataLogManager.getLog().start("odom period", "double");
+  }
+
+  /* Accept the swerve drive state and telemeterize it to smartdashboard */
+  public void telemeterize(SwerveDriveState state) {
+    /* Telemeterize the pose */
+    Pose2d pose = state.Pose;
+    fieldTypePub.set("Field2d");
+    fieldPub.set(new double[] {
+        pose.getX(),
+        pose.getY(),
+        pose.getRotation().getDegrees()
+    });
+
+    /* Telemeterize the robot's general speeds */
+    double currentTime = Utils.getCurrentTimeSeconds();
+    double diffTime = currentTime - lastTime;
+    lastTime = currentTime;
+    Translation2d distanceDiff = pose.minus(lastPose).getTranslation();
+    lastPose = pose;
+
+    Translation2d velocities = distanceDiff.div(diffTime);
+
+    speed.set(velocities.getNorm());
+    velocityX.set(velocities.getX());
+    velocityY.set(velocities.getY());
+    odomPeriod.set(1.0 / state.OdometryPeriod);
+
+    /* Telemeterize the module's states */
+    for (int i = 0; i < 4; ++i) {
+      moduleSpeeds[i].setAngle(state.ModuleStates[i].angle);
+      moduleDirections[i].setAngle(state.ModuleStates[i].angle);
+      moduleSpeeds[i].setLength(state.ModuleStates[i].speedMetersPerSecond / (2 * maxSpeed));
+
+      SmartDashboard.putData("Module " + i, moduleMechanisms[i]);
     }
 
-    /* What to publish over networktables for telemetry */
-    NetworkTableInstance inst = NetworkTableInstance.getDefault();
+    DataLogManager.getLog().appendDoubleArray(logEntry,
+        new double[] { pose.getX(), pose.getY(), pose.getRotation().getDegrees() },
+        (long) (Timer.getFPGATimestamp() * 1000000));
+    DataLogManager.getLog().appendDouble(odomEntry, state.OdometryPeriod, (long) (Timer.getFPGATimestamp() * 1000000));
 
-    /* Robot pose for field positioning */
-    NetworkTable table = inst.getTable("Pose");
-    DoubleArrayPublisher fieldPub = table.getDoubleArrayTopic("robotPose").publish();
-    StringPublisher fieldTypePub = table.getStringTopic(".type").publish();
-
-    /* Robot speeds for general checking */
-    NetworkTable driveStats = inst.getTable("Drive");
-    DoublePublisher velocityX = driveStats.getDoubleTopic("Velocity X").publish();
-    DoublePublisher velocityY = driveStats.getDoubleTopic("Velocity Y").publish();
-    DoublePublisher speed = driveStats.getDoubleTopic("Speed").publish();
-    DoublePublisher odomPeriod = driveStats.getDoubleTopic("Odometry Period").publish();
-
-    /* Keep a reference of the last pose to calculate the speeds */
-    Pose2d m_lastPose = new Pose2d();
-    double lastTime = Utils.getCurrentTimeSeconds();
-
-    /* Mechanisms to represent the swerve module states */
-    Mechanism2d[] m_moduleMechanisms = new Mechanism2d[] {
-            new Mechanism2d(1, 1),
-            new Mechanism2d(1, 1),
-            new Mechanism2d(1, 1),
-            new Mechanism2d(1, 1),
-    };
-    /* A direction and length changing ligament for speed representation */
-    MechanismLigament2d[] m_moduleSpeeds = new MechanismLigament2d[] {
-            m_moduleMechanisms[0].getRoot("RootSpeed", 0.5, 0.5).append(new MechanismLigament2d("Speed", 0.5, 0)),
-            m_moduleMechanisms[1].getRoot("RootSpeed", 0.5, 0.5).append(new MechanismLigament2d("Speed", 0.5, 0)),
-            m_moduleMechanisms[2].getRoot("RootSpeed", 0.5, 0.5).append(new MechanismLigament2d("Speed", 0.5, 0)),
-            m_moduleMechanisms[3].getRoot("RootSpeed", 0.5, 0.5).append(new MechanismLigament2d("Speed", 0.5, 0)),
-    };
-    /* A direction changing and length constant ligament for module direction */
-    MechanismLigament2d[] m_moduleDirections = new MechanismLigament2d[] {
-            m_moduleMechanisms[0].getRoot("RootDirection", 0.5, 0.5)
-                    .append(new MechanismLigament2d("Direction", 0.1, 0, 0, new Color8Bit(Color.kWhite))),
-            m_moduleMechanisms[1].getRoot("RootDirection", 0.5, 0.5)
-                    .append(new MechanismLigament2d("Direction", 0.1, 0, 0, new Color8Bit(Color.kWhite))),
-            m_moduleMechanisms[2].getRoot("RootDirection", 0.5, 0.5)
-                    .append(new MechanismLigament2d("Direction", 0.1, 0, 0, new Color8Bit(Color.kWhite))),
-            m_moduleMechanisms[3].getRoot("RootDirection", 0.5, 0.5)
-                    .append(new MechanismLigament2d("Direction", 0.1, 0, 0, new Color8Bit(Color.kWhite))),
-    };
-
-    DoubleArrayPublisher moduleStatePublisher = driveStats.getDoubleArrayTopic("Module States").publish();
-
-    /* Accept the swerve drive state and telemeterize it to smartdashboard */
-    public void telemeterize(SwerveDriveState state) {
-        /* Telemeterize the pose */
-        Pose2d pose = state.Pose;
-        fieldTypePub.set("Field2d");
-        fieldPub.set(new double[] {
-                pose.getX(),
-                pose.getY(),
-                pose.getRotation().getDegrees()
-        });
-
-        /* Telemeterize the robot's general speeds */
-        double currentTime = Utils.getCurrentTimeSeconds();
-        double diffTime = currentTime - lastTime;
-        lastTime = currentTime;
-        Translation2d distanceDiff = pose.minus(m_lastPose).getTranslation();
-        m_lastPose = pose;
-
-        Translation2d velocities = distanceDiff.div(diffTime);
-
-        speed.set(velocities.getNorm());
-        velocityX.set(velocities.getX());
-        velocityY.set(velocities.getY());
-        odomPeriod.set(1.0 / state.OdometryPeriod);
-
-        /* Telemeterize the module's states */
-        for (int i = 0; i < 4; ++i) {
-            m_moduleSpeeds[i].setAngle(state.ModuleStates[i].angle);
-            m_moduleDirections[i].setAngle(state.ModuleStates[i].angle);
-            m_moduleSpeeds[i].setLength(state.ModuleStates[i].speedMetersPerSecond / (2 * MaxSpeed));
-
-            SmartDashboard.putData("Module " + i, m_moduleMechanisms[i]);
-        }
-
-        DataLogManager.getLog().appendDoubleArray(logEntry, new double[] {pose.getX(), pose.getY(), pose.getRotation().getDegrees()}, (long)(Timer.getFPGATimestamp() * 1000000));
-        DataLogManager.getLog().appendDouble(odomEntry, state.OdometryPeriod, (long)(Timer.getFPGATimestamp() * 1000000));
-
-        // Publish module states in AdvantageScope format
-        var moduleStates = new double[state.ModuleStates.length * 2];
-        for (int i = 0; i < state.ModuleStates.length; i++) {
-          var moduleState = state.ModuleStates[i];
-          moduleStates[i * 2] = moduleState.angle.getDegrees();
-          moduleStates[i * 2 + 1] = moduleState.speedMetersPerSecond;
-        }
-        moduleStatePublisher.set(moduleStates);
-
+    // Publish module states in AdvantageScope format
+    var moduleStates = new double[state.ModuleStates.length * 2];
+    for (int i = 0; i < state.ModuleStates.length; i++) {
+      var moduleState = state.ModuleStates[i];
+      moduleStates[i * 2] = moduleState.angle.getDegrees();
+      moduleStates[i * 2 + 1] = moduleState.speedMetersPerSecond;
     }
+    moduleStatePublisher.set(moduleStates);
+
+  }
 }
