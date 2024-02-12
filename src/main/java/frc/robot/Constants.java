bf2a14597bebbf549ba81d3d1e86e51239dadb94
Finn Sherman <finnsherman999@gmail.com>
2024-02-12 09:24:17 -0600

    added slot configs for shooter subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -30,9 +30,10 @@ public class Constants {
 
     // Theoretical free speed of L2 Falcon 500 with FOC
     public static final Measure<Velocity<Distance>> MAX_VELOCITY = FeetPerSecond.of(15.7);
-        // Here we calculate the theoretical maximum angular velocity. You can also replace this with a measured amount.
-        public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = 
-        (DrivetrainConstants.MAX_VELOCITY.in(MetersPerSecond) /
+    // Here we calculate the theoretical maximum angular velocity. You can also
+    // replace this with a measured amount.
+    public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = (DrivetrainConstants.MAX_VELOCITY
+        .in(MetersPerSecond) /
         Math.hypot(TRACKWIDTH.in(Meters) / 2.0, WHEELBASE.in(Meters) / 2.0));
     public static final Measure<Velocity<Angle>> MAX_ANGULAR_VELOCITY = RadiansPerSecond.of(PI * 4);
   }
@@ -41,10 +42,9 @@ public class Constants {
 
     public static final double DEADBAND = 0.1;
 
-    public static final Measure<Velocity<Velocity<Distance>>> TRANSLATION_RATE_LIMIT =
-        MetersPerSecondPerSecond.of(8.0);
-    public static final Measure<Velocity<Velocity<Angle>>> ROTATION_RATE_LIMIT =
-        RadiansPerSecond.per(Second).of(8.0 * PI);
+    public static final Measure<Velocity<Velocity<Distance>>> TRANSLATION_RATE_LIMIT = MetersPerSecondPerSecond.of(8.0);
+    public static final Measure<Velocity<Velocity<Angle>>> ROTATION_RATE_LIMIT = RadiansPerSecond.per(Second)
+        .of(8.0 * PI);
 
   }
 
@@ -64,11 +64,14 @@ public class Constants {
 
     public static final String APRILTAG_CAMERA_NAME = "OV9281";
 
-    /** Physical location of the apriltag camera on the robot, relative to the center of the robot. */
+    /**
+     * Physical location of the apriltag camera on the robot, relative to the center
+     * of the robot.
+     */
     public static final Transform3d APRILTAG_CAMERA_TO_ROBOT = new Transform3d(
         new Translation3d(-0.06, 0.2, -0.2127),
         new Rotation3d(0.0, degreesToRadians(15.0), degreesToRadians(-3.0)));
-    
+
     public static final double FIELD_LENGTH_METERS = 16.54175;
     public static final double FIELD_WIDTH_METERS = 8.0137;
 
@@ -87,6 +90,16 @@ public class Constants {
         .withKP(0.59)
         .withKI(0)
         .withKD(0.001)
+        .withKS(0);
+    public static final SlotConfigs ALTITUDE_SLOT_CONFIGS = new SlotConfigs()
+        .withKP(0)
+        .withKI(0)
+        .withKD(0)
+        .withKS(0);
+    public static final SlotConfigs SHOOTER_INTAKE_SLOT_CONFIGS = new SlotConfigs()
+        .withKP(0)
+        .withKI(0)
+        .withKD(0)
         .withKS(0);
   }
 
@@ -109,6 +122,6 @@ public class Constants {
 
   public static class TrapAmpShooterConstants {
     public static final int DEVICE_ID_TRAP_AMP_SHOOTER = 10;
-    
+
   }
 }
