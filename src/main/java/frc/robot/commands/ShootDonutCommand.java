f2fef4249f43c9dcc4b383bff142eb6de6bef4d0
Finn Sherman <finnsherman999@gmail.com>
2024-02-14 15:46:51 -0600

    updated for lack of wrist

--- src/main/java/frc/robot/commands/ShootDonutCommand.java
+++ src/main/java/frc/robot/commands/ShootDonutCommand.java
@@ -1,19 +1,7 @@
 package frc.robot.commands;
 
-import static edu.wpi.first.units.Units.Radians;
-import static frc.robot.Constants.AutoDriveConstants.THETA_kD;
-import static frc.robot.Constants.AutoDriveConstants.THETA_kI;
-import static frc.robot.Constants.AutoDriveConstants.THETA_kP;
-
-import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
-import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.SteerRequestType;
-import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
-import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest.ForwardReference;
-import com.ctre.phoenix6.mechanisms.swerve.utility.PhoenixPIDController;
-
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
@@ -23,38 +11,27 @@ import frc.robot.subsystems.ShooterSubsystem;
 public class ShootDonutCommand extends Command {
 
   private static final double SHOOT_TIME = 5;
-  private final CommandSwerveDrivetrain drivetrain;
   private final ShooterSubsystem shooterSubsystem;
   private final IndexerSubsystem indexerSubsystem;
-  private final SwerveRequest.FieldCentricFacingAngle swerveRequest = new SwerveRequest.FieldCentricFacingAngle()
-    .withDriveRequestType(DriveRequestType.Velocity)
-    .withSteerRequestType(SteerRequestType.MotionMagic);
-
 
   private final Timer shootTimer = new Timer();
 
-  public ShootDonutCommand(ShooterSubsystem shooterSubsystem, IndexerSubsystem indexerSubsystem, 
-  CommandSwerveDrivetrain drivetrain) {
+  public ShootDonutCommand(ShooterSubsystem shooterSubsystem, IndexerSubsystem indexerSubsystem) {
 
-    this.drivetrain = drivetrain;
     this.shooterSubsystem = shooterSubsystem;
     this.indexerSubsystem = indexerSubsystem;
 
-    addRequirements(indexerSubsystem, shooterSubsystem, drivetrain);
-
-    swerveRequest.ForwardReference = ForwardReference.RedAlliance;
-    swerveRequest.HeadingController = new PhoenixPIDController(THETA_kP, THETA_kI, THETA_kD);
+    addRequirements(indexerSubsystem, shooterSubsystem);
   }
 
   @Override
   public void initialize() {
-    swerveRequest.HeadingController.reset();
     shootTimer.reset();
   }
 
   @Override
   public void execute() {
-    shooterSubsystem.setAimPosition(Radians.of(Math.PI));
+    indexerSubsystem.intake();
     shooterSubsystem.spinShooterWheel(40);
     if (shooterSubsystem.isShooterReady())
       indexerSubsystem.shoot();
@@ -68,7 +45,6 @@ public class ShootDonutCommand extends Command {
 
   @Override
   public void end(boolean interrupted) {
-    shooterSubsystem.actuatorStop();
     indexerSubsystem.stop();
     shooterSubsystem.stop();
     shootTimer.stop();
