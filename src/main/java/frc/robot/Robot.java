97423352e56facbd943303af393a469eb0c01d5f
Andrew Gasser <agasser01@hotmail.com>
2024-11-03 15:50:05 -0600

    Add DS logging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import com.ctre.phoenix6.SignalLogger;
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -21,6 +22,8 @@ public class Robot extends TimedRobot {
     m_robotContainer = new RobotContainer();
 
     SignalLogger.start(); // CTRE logger
+    DataLogManager.start(); // WPILib logger
+    DriverStation.startDataLog(DataLogManager.getLog()); // Record both DS control and joystick data
   }
 
   @Override
